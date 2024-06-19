ea85606d6e3ecdfba315375240db1ace84788bbd
webdevmahabub <mahbubbd926@gmail.com>
2024-06-19 17:56:58 +0600

    Route & Login Register Setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
-import './App.css';
+import { useState } from "react";
+import Router from "./router/Router";
+import publicRoutes from "./router/routes/publicRoutes";
 
 function App() {
-  return (
-    <div className="App">
-      <h1 className="text-3xl font-bold underline text-green-600	">
-              GreenViewSoft
-      </h1>
-    </div>
-  );
+  const [allRoutes, setAllRoutes] = useState([...publicRoutes])
+    console.log(allRoutes)
+
+
+    return <Router allRoutes={allRoutes} /> 
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,14 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React,{lazy,Suspense} from 'react';
+import ReactDOM from 'react-dom/client'; 
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter } from 'react-router-dom';
+
+const App = lazy(() => import('./App'))
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+    <BrowserRouter>
+    <Suspense>
     <App />
-  </React.StrictMode>
+    </Suspense>
+    </BrowserRouter>
 );
 
 // If you want to start measuring performance in your app, pass a function

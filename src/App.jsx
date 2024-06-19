bebbd45bbc4b7d6c4eec27152f11a5f325dd0f93
webdevmahabub <mahbubbd926@gmail.com>
2024-06-19 17:05:00 +0600

    First Commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className="text-3xl font-bold underline text-green-600	">
+              GreenViewSoft
+      </h1>
+    </div>
+  );
+}
+
+export default App;

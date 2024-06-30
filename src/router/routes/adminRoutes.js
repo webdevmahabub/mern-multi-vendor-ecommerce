0f6e5bd53923737f8d092d8dec46e325bd73bc70
webdevmahabub <mahbubbd926@gmail.com>
2024-06-30 23:24:23 +0600

    Category & Seller Page Design

--- src/router/routes/adminRoutes.js
+++ src/router/routes/adminRoutes.js
@@ -1,6 +1,8 @@
 import { lazy } from "react";    
+import Category from "../../views/admin/Category";
 const AdminDashboard = lazy(()=> import('../../views/admin/AdminDashboard'))  
 const Orders = lazy(()=> import('../../views/admin/Orders'))    
+const Sellers = lazy(()=> import('../../views/admin/Sellers'))
 
 export const adminRoutes = [
     {
@@ -12,6 +14,16 @@ export const adminRoutes = [
         path: 'admin/dashboard/orders',
         element : <Orders/> ,
         role : 'admin'
-    }
+    },
+    {
+        path: 'admin/dashboard/Category',
+        element : <Category/>,
+        role : 'admin'
+    },
+    {
+        path: 'admin/dashboard/Sellers',
+        element : <Sellers/>,
+        role : 'admin'
+    },
 
 ]

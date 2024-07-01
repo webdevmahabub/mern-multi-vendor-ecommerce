17a45622c03265206bff2e39727488d70a2b9a05
webdevmahabub <mahbubbd926@gmail.com>
2024-07-01 22:54:50 +0600

    Owner Order Details Page Design part 2

--- src/views/admin/OrderDetails.jsx
+++ src/views/admin/OrderDetails.jsx
@@ -56,11 +56,115 @@ const OrderDetails = () => {
                         <div className='flex flex-col gap-1'>
 
                             <h2 className='pb-2 font-semibold'>Deliver To : Ramjan Bapari </h2>
-
-
-
+                            <p><span className='text-sm'>71 siddheshwari circular road, flat #4-b/2, talukder estate (opposite to shantinagar post office), </span></p> 
 
                         </div>
+
+                        <div className='flex justify-start items-center gap-3'>
+                <h2>Payment Status: </h2>
+                <span className='text-base'>Paid</span>
+             </div>  
+             <span>Price : $232</span> 
+
+            <div className='mt-4 flex flex-col gap-4 bg-[#8288ed] rounded-md'>
+                <div className='text-[#d0d2d6]'>
+                    <div className='flex gap-3 text-md'>
+                        <img className='w-[50px] h-[50px]' src="http://localhost:3000/images/category/1.jpg" alt="" />
+
+                        <div>
+                            <h2>Addidas Fly Shoes </h2>
+                            <p>
+                                <span>Brand : </span>
+                                <span>Easy</span>
+                                <span className='text-lg'>Quantity : 3 </span>
+                            </p>
+                        </div> 
+                    </div> 
+                </div>
+                </div>  
+
+
+                <div className='mt-4 flex flex-col gap-4 bg-[#8288ed] rounded-md'>
+                <div className='text-[#d0d2d6]'>
+                    <div className='flex gap-3 text-md'>
+                        <img className='w-[50px] h-[50px]' src="http://localhost:3000/images/category/1.jpg" alt="" />
+
+                        <div>
+                            <h2>Addidas Fly Shoes</h2>
+                            <p>
+                                <span>Brand : </span>
+                                <span>Easy</span>
+                                <span className='text-lg'>Quantity : 3 </span>
+                            </p>
+                        </div> 
+                    </div> 
+                </div>
+                </div>  
+
+
+                <div className='mt-4 flex flex-col gap-4 bg-[#8288ed] rounded-md'>
+                <div className='text-[#d0d2d6]'>
+                    <div className='flex gap-3 text-md'>
+                        <img className='w-[50px] h-[50px]' src="http://localhost:3000/images/category/1.jpg" alt="" />
+
+                        <div>
+                            <h2>Addidas Fly Shoes </h2>
+                            <p>
+                                <span>Brand : </span>
+                                <span>Easy</span>
+                                <span className='text-lg'>Quantity : 3 </span>
+                            </p>
+                        </div> 
+                    </div> 
+                </div>
+                </div>  
+
+
+
+                    </div>
+                </div> 
+
+    <div className='w-[70%]'>
+        <div className='pl-3'>
+            <div className='mt-4 flex flex-col bg-[#8288ed] rounded-md p-4'>
+                <div className='text-[#d0d2d6] mt-2'>
+                    <div className='flex justify-start items-center gap-3'>
+                        <h2>Seller 1 Order : </h2>
+                        <span>pending</span> 
+                    </div>
+                    <div className='flex gap-3 text-md mt-2'>
+                        <img className='w-[50px] h-[50px]' src="http://localhost:3000/images/category/1.jpg" alt="" />
+
+                        <div>
+                            <h2>Addidas Fly Shoes </h2>
+                            <p>
+                                <span>Brand : </span>
+                                <span>Easy</span>
+                                <span className='text-lg'>Quantity : 3 </span>
+                            </p>
+                        </div> 
+                    </div> 
+
+                </div>
+
+
+                <div className='text-[#d0d2d6] mt-2'>
+                    <div className='flex justify-start items-center gap-3'>
+                        <h2>Seller 1 Order : </h2>
+                        <span>pending</span> 
+                    </div>
+                    <div className='flex gap-3 text-md mt-2'>
+                        <img className='w-[50px] h-[50px]' src="http://localhost:3000/images/category/1.jpg" alt="" />
+
+                        <div>
+                            <h2>Addidas Fly Shoes </h2>
+                            <p>
+                                <span>Brand : </span>
+                                <span>Easy</span>
+                                <span className='text-lg'>Quantity : 3 </span>
+                            </p>
+                        </div> 
+                    </div> 
 
 
 
@@ -75,7 +179,10 @@ const OrderDetails = () => {
             </div>
 
 
+        
 
+</div>   
+</div> 
 
 
 

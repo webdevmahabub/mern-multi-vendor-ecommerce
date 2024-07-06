c9e21c65139eca18d39fefa8f800fffb51b322fa
webdevmahabub <mahbubbd926@gmail.com>
2024-07-07 00:00:48 +0600

    Orders & Payments Page Design

--- src/router/routes/sellerRoutes.js
+++ src/router/routes/sellerRoutes.js
@@ -1,9 +1,11 @@
 import { lazy } from "react"; 
+const Orders = lazy(()=> import('../../views/seller/Orders'))
 const Products = lazy(()=> import('../../views/seller/Products'))
 const AddProduct = lazy(()=> import('../../views/seller/AddProduct'))
 const Home = lazy(()=> import('../../views/Home'))   
 const SellerDashboard = lazy(()=> import('../../views/seller/SellerDashboard'))
 const DiscountProducts = lazy(()=> import('../../views/seller/DiscountProducts'))
+const Payments = lazy(()=> import('../../views/seller/Payments'))
 
 export const sellerRoutes = [
     {
@@ -29,6 +31,16 @@ export const sellerRoutes = [
     {
         path: '/seller/dashboard/discount-product',
         element : <DiscountProducts/>,
+        ability : ['seller']
+    },
+    {
+        path: '/seller/dashboard/orders',
+        element : <Orders/>,
+        ability : ['seller']
+    },
+    {
+        path: '/seller/dashboard/payments',
+        element : <Payments/>,
         ability : ['seller']
     }
 

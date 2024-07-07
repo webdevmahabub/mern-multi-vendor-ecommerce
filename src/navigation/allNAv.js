f607fdbee26d156ab93bd6a347d4dd373cd1788b
webdevmahabub <mahbubbd926@gmail.com>
2024-07-07 19:33:31 +0600

    Seller Profile Page design

--- src/navigation/allNAv.js
+++ src/navigation/allNAv.js
@@ -117,5 +117,13 @@ export const allNav = [
         icon : <AiOutlineDashboard />,
         role : 'seller',
         path: '/seller/dashboard/chat-support'
+    },
+    {
+        id : 17,
+        title : 'Profile',
+        icon : <AiOutlineDashboard />,
+        role : 'seller',
+        path: '/seller/dashboard/profile'
+        
     }
 ]
--- src/router/routes/sellerRoutes.js
+++ src/router/routes/sellerRoutes.js
@@ -1,10 +1,11 @@
-import { lazy } from "react";        
-const Home = lazy(()=> import('../../views/Home'))   
-const SellerDashboard = lazy(()=> import('../../views/seller/SellerDashboard'))   
-const AddProduct = lazy(()=> import('../../views/seller/AddProduct'))   
-const Products = lazy(()=> import('../../views/seller/Products')) 
-const DiscountProducts = lazy(()=> import('../../views/seller/DiscountProducts')) 
-const Orders = lazy(()=> import('../../views/seller/Orders')) 
+import { lazy } from "react";
+const Profile = lazy(()=> import('../../views/seller/Profile'))
+const Home = lazy(()=> import('../../views/Home'))
+const SellerDashboard = lazy(()=> import('../../views/seller/SellerDashboard'))
+const AddProduct = lazy(()=> import('../../views/seller/AddProduct'))
+const Products = lazy(()=> import('../../views/seller/Products'))
+const DiscountProducts = lazy(()=> import('../../views/seller/DiscountProducts'))
+const Orders = lazy(()=> import('../../views/seller/Orders'))
 const Payments = lazy(()=> import('../../views/seller/Payments'))
 const SellerToAdmin = lazy(()=> import('../../views/seller/SellerToAdmin'))
 const SellerToCustomer = lazy(()=> import('../../views/seller/SellerToCustomer'))
@@ -65,6 +66,12 @@ export const sellerRoutes = [
     {
         path: '/seller/dashboard/chat-customer',
         element : <SellerToCustomer/>,
+        role : 'seller',
+        status : 'active'
+    },
+    {
+        path: '/seller/dashboard/profile',
+        element : <Profile/>,
         role : 'seller',
         status : 'active'
     }
